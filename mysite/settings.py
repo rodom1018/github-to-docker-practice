bd9ed9470b3b797001e86bc81e99bd069b88ce8e
rodom1018 <76900144+rodom1018@users.noreply.github.com>
2022-08-24 17:24:04 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--%_blo2r+irup+3976%__prc%-%01whhw+@8you&t@jx8^q)_c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
